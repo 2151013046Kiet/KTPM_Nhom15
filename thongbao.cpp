d221ae5f8463a3ed4b3139807a03d9cd7200f848
thanh binh <binh1082003@gmail.com>
2023-10-23 13:32:46 +0700

    70 thay doi noi dung file thongbao.cpp vao nhanh feature-new

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!";
-	std::cout << "thong bao doi noi dung
-!";
+	
+	std::cout << "thong bao doi noi dung !";
+	std::cout << "thay doi noi dung!";
+
 
 	return 0;
 }
